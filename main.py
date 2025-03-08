cde9dabd47da8337aeca73684c275b3ed114ff83
Azhar Harisandi <azharharisandi@gmail.com>
2025-03-08 22:59:05 +0700

    complete report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, characters_count
 
 def get_book_text(filepath):
     with open(filepath) as f: 
@@ -7,9 +7,41 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    contents = get_book_text("./books/frankenstein.txt")
+    import sys 
+
+    if len(sys.argv) < 2:
+        print("""
+Please provide book path as an argument
+              
+Usage : python3 main.py path_to_book
+              """)
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    contents = get_book_text(book_path)
     count_words = get_num_words(contents)
-    print(f"{count_words} words found in the document")
+    chars_count = sorted(characters_count(contents).items(), 
+                         key=lambda x: x[1], 
+                         reverse=True)
+    chars_count_report = ""
+    for chars in chars_count:
+        if chars[0].isalpha():
+            chars_count_report += f"{chars[0]}: {chars[1]}\n"
+    
+    r = report(book_path, count_words, chars_count_report.rstrip())
+    print(r)
+
+
+
+def report(book_path: str, word_count: int, character_count: dict[str, int]) -> str:
+    return f"""============ BOOKBOT ============
+Analyzing book found at {book_path}...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------
+{character_count}
+============= END ===============
+    """
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
+from collections import Counter
+
+
 def get_num_words(string):
     ## count number of words from a string
     return len(string.split())
+
+def characters_count(string):
+    result = {} 
+    string = string.lower()
+    for s in string: 
+        count = result.get(s, None)
+        if count:
+            result[s] = count + 1
+        else:
+            result[s] = 1
+        
+    return result
+    # return dict(Counter(string.lower()))
+
+
+if __name__ == "__main__":
+    a = characters_count("geeks for geeks")
+    print(a)
